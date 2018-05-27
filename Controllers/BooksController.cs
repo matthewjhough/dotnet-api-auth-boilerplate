e3f5da89f1720e6922f2495627b946107bf65e26
matthewjhough <matthough1@icloud.com>
2018-05-27 12:23:06 -0400

    backend c-sharp API with authentication (auth0, JWT)

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using mgm_app.Models;
+
+namespace mgm_app.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet, Authorize]
+        public IEnumerable<Book> Get() {
+            var currentUser = HttpContext.User;
+            var resultBookList = new Book[] {
+                new Book { 
+                    Author = "Ray Bradbury", 
+                    Title = "Fahrenheit 451", 
+                    AgeRestriction = false 
+                },
+                new Book { 
+                    Author = "Philip K Dick", 
+                    Title = "Do androids dream of electric sheep?", 
+                    AgeRestriction = false 
+                },
+                new Book { 
+                    Author = "George Orwell", 
+                    Title = "1984", 
+                    AgeRestriction = false 
+                },
+                new Book { 
+                    Author = "Anais Nin", 
+                    Title = "Delta of Venus",
+                    AgeRestriction = true 
+                }
+            };
+
+            return resultBookList;
+        }
+
+    }
+}
